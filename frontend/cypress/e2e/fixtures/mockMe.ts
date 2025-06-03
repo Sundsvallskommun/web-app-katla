6ec5970ffdc26ff9a9804b038824f720234f00e7
Henrik Sandstrom <henrik.sandstrom@sundsvall.se>
2025-06-03 07:28:33 +0200

    E2e test foundation

---
+++ frontend/cypress/e2e/fixtures/mockMe.ts
@@ -0,0 +1,12 @@
+export const mockMe = {
+  data: {
+    name: 'My Testsson',
+    firstName: 'My',
+    lastName: 'Testsson',
+    username: 'kctest',
+    userSettings: {
+      readNotificationsClearedDate: '2023-10-19T08:39:41.543Z',
+    },
+  },
+  message: 'success',
+};
