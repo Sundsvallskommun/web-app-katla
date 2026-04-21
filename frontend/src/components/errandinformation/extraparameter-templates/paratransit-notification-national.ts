54de17d44aca7b78c5e49130ab42045ba561592e
OliverBorgstrom <oliver.borgstrom@sundsvall.se>
2026-04-21 11:38:34 +0200

    feat: updated extraparameter

--- frontend/src/components/errandinformation/extraparameter-templates/paratransit-notification-national.ts
+++ frontend/src/components/errandinformation/extraparameter-templates/paratransit-notification-national.ts
@@ -154,7 +154,7 @@ export const notificationNational_UppgiftFieldTemplate: UppgiftFieldExtended[] =
   {
     field: 'personal.mobilityAids',
     value: [],
-    label: 'Ange ett eller flera förflyttningshjälpmedel som den sökande är beroende av för att kunna genomföra resan',
+    label: 'Välj förflyttningshjälpmedel som den sökande är beroende av under själva resan',
     formField: {
       type: 'combobox',
       options: [
@@ -179,7 +179,27 @@ export const notificationNational_UppgiftFieldTemplate: UppgiftFieldExtended[] =
       },
     ],
   },
-    {
+  {
+    field: 'personal.wheelchairDimensions',
+    value: '',
+    label: 'Ange information om rullstolens yttermått, längd och bredd, samt totalvikten när personen sitter i rullstolen',
+    formField: {
+      type: 'textarea',
+    },
+    section: 'Yttre omständigheter',
+    dependsOnLogic: 'OR',
+    dependsOn: [
+      {
+        field: 'personal.mobilityAids',
+        value: 'COMFORT_WHEELCHAIR',
+      },
+      {
+        field: 'personal.mobilityAids',
+        value: 'ELECTRIC_WHEELCHAIR',
+      },
+    ],
+  },
+  {
     field: 'personal.needForEscort',
     value: '',
     label: 'Behöver den sökande aktiv hjälp av en ledsagare under själva resan',
